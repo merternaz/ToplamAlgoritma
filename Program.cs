ae32e01e5d7f7d449d5ddecfb7e85b65ac4dc959
Mert Ernaz <rnzappdroid@gmail.com>
2023-05-25 00:27:02 +0300

    toplam algoritması

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace structures
+{
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kac adet sayı girilecek ? :");
+            int x=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            List<int> entry=new List<int>();
+            for(int i=0;i<x;i++){
+                int y=Convert.ToInt32(Console.ReadLine());
+                entry.Add(y);
+            }
+
+            double showNum=0;
+
+            for(int a=0;a<entry.Count;a++){
+                if(a>0 && a%2!=0){
+                    if(entry[a]!=entry[a-1]){
+                        showNum=entry[a]+entry[a-1];
+                    }else{
+                        showNum=Math.Pow(entry[a]+entry[a],2);
+                    }
+                    Console.Write(showNum+" ");
+                }
+
+                
+                
+            }
+        }
+    }
+}
